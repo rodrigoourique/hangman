4dac9c03625715e2eddcb583a7337dc062eff1eb
Cátia Gouveia <cssgouveia31@gmail.com>
2019-07-13 18:44:10 +0100

    add a listener thread to Client class and a little debug in other class

--- src/org/academiadecodigo/codezillas/Player/Client.java
+++ src/org/academiadecodigo/codezillas/Player/Client.java
@@ -1,4 +1,32 @@
 package org.academiadecodigo.codezillas.Player;
 
-public class Client {
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client implements Runnable {
+
+    private final PrintWriter outPut;
+    private final Scanner userEntry;
+
+    public Client(Socket socket) throws IOException {
+        this.userEntry = new Scanner(System.in);
+        outPut = new PrintWriter(socket.getOutputStream(), true);
+    }
+
+    @Override
+    public void run() {
+        String message;
+
+        while (true) {
+            message = userEntry.nextLine();
+            outPut.println(message);
+
+            if (!(message.equals("-") || message.equals("no"))) {
+                System.out.println("\nClosing connection...");
+                System.exit(0);
+            }
+        }
+    }
 }
