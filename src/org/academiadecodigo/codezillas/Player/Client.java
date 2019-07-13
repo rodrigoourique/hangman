b03dbed75fccb21aeb6f8b7ef9fccedf9ef61c54
Ana Garcia <anasofiapamplonagarcia@gmail.com>
2019-07-13 18:25:53 +0000

    Added initSocket class to Client class and a little debug

--- src/org/academiadecodigo/codezillas/Player/Client.java
+++ src/org/academiadecodigo/codezillas/Player/Client.java
@@ -2,6 +2,7 @@ package org.academiadecodigo.codezillas.Player;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.net.InetAddress;
 import java.net.Socket;
 import java.util.Scanner;
 
@@ -15,6 +16,7 @@ public class Client implements Runnable {
         outPut = new PrintWriter(socket.getOutputStream(), true);
     }
 
+
     @Override
     public void run() {
         String message;
@@ -27,6 +29,27 @@ public class Client implements Runnable {
                 System.out.println("\nClosing connection...");
                 System.exit(0);
             }
+        }
+    }
+    public class InitSocket {
+        private Scanner write = new Scanner(System.in);
+        private Socket socket;
+        private final int PORT = 8080;
+        private String HOST;
+
+        public InitSocket() throws IOException{
+            System.out.println("Hey! Before you start, please enter your IP Address! (If you are a localhost, press Enter.)");
+            Scanner host = new Scanner(System.in);
+            HOST = host.nextLine();
+
+            if(HOST == null){
+                HOST = InetAddress.getLocalHost().toString();
+            }
+            socket = new Socket(HOST, PORT);
+        }
+
+        public Socket getSocket() {
+            return this.socket;
         }
     }
 }
